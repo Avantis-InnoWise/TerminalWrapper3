fb5abe9698409e11be2180e9d4661f9c3b858ea0
Alexander Usnarsky <aliaksandr.usnarski@innowise-group.com>
2022-03-29 14:12:28 +0300

    Refactoring and rename property.

--- TerminalWrapper3/main.swift
+++ TerminalWrapper3/main.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-let cdNa = Commands.mtdDec(with key: "63FGG28", slt: Commands.saltVl)
+let dnst = Commands.mtdDec(with: "63FGG28", slat: Commands.saltVl)
 
 let errorHandler = ErrorHandler()
 
 do {
-    let result = try errorHandler.transformation(cdNa, ComOp: .commandOne)
+    let result = try errorHandler.transformation(dnst, ComOp: .commandOne)
     print(result)
 } catch {
     print("Launch error")
